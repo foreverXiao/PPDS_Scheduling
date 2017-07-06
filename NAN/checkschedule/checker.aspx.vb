8c703ad988d01381fc81b5194d9e106751300df3
foreverXiao <forever_xiao@aliyun.com>
2017-07-06 17:39:30 +0800

    Add files via upload

---
+++ NAN/checkschedule/checker.aspx.vb
@@ -0,0 +1,17 @@
+﻿
+Partial Class checkschedule_checker
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim cookiePath As String = Request.ApplicationPath
+
+        If (Request.Cookies("userInfo") Is Nothing) OrElse (CType(Server.HtmlEncode(Request.Cookies("userInfo")("level")), Integer) < 4) OrElse (Request.Cookies(cookiePath) Is Nothing) Then
+            '(Request.Cookies("userInfo") Is Nothing) OrElse String.IsNullOrEmpty(Request.Cookies("userInfo")("level")) OrElse (CType(Request.Cookies("userInfo")("level"), Integer) < accesslevel())) 
+            Response.Redirect("loginchc.aspx?oldurl=checker.aspx")
+        End If
+    End Sub
+
+
+
+End Class
