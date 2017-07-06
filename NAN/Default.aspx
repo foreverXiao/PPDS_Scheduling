818c120cfd0836073f69d86052fedaa21e8c59d4
foreverXiao <forever_xiao@aliyun.com>
2017-07-06 17:18:00 +0800

    Add files via upload

---
+++ NAN/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPages/MasterPage.master" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" Theme="Monochrome" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CP1" Runat="Server">
+</asp:Content>
+
---
+++ NAN/Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Imports basepage1
+
+Partial Class _Default
+    Inherits basepage1
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Redirect("~/dragDrop/OrderDetail.aspx")
+        Dim actionRequested As String = Request.Params("title")
+        If Not String.IsNullOrEmpty(actionRequested) Then Page.Title = actionRequested
+    End Sub
+End Class
